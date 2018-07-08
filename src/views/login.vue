aaefdc1480ba09b5126edd794bcda7e07c6d56e8
xiepengfeijing <734332894@qq.com>
2018-07-08 10:33:38 +0800

    登录-搭建界面

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,15 +1,50 @@
 <template>
-    <div>
-        Login
+    <div class="login-wrap">
+        <el-form
+        class="login-form"
+        label-position="top"
+        label-width="80px"
+        :model="formData">
+            <h2>用户登录</h2>
+            <el-form-item label="用户名">
+                <el-input v-model="formData.username"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+                <el-input type="password" v-model="formData.password"></el-input>
+            </el-form-item>
+            <el-button class="login-button" type="primary">主要按钮</el-button>
+        </el-form>
     </div>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return {
+            formData: {
+                username: '',
+                password: ''
+            }
+        };
+    }
 };
 </script>
 
 <style>
-
+    .login-wrap {
+        background-color: #324152;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .login-wrap .login-form {
+        background-color: #fff;
+        width: 400px;
+        padding: 30px;
+        border-radius: 5px;
+    }
+    .login-button{
+        width: 100%;
+    }
 </style>
