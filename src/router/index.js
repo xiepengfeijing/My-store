f8ba45382a7d0c8d256e189c3c7ab7d028420bc5
xiepengfeijing <734332894@qq.com>
2018-07-08 10:18:15 +0800

    登录-配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Login from '@/views/login';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
+      name: 'login',
+      path: '/login',
+      component: Login
+    },
+    {
+      name: 'home',
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: {name: 'login'}
     }
   ]
 });
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,12 +1,11 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div>
+        Login
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
 
 };
 </script>
