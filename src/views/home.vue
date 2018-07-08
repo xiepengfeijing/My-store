445a5de3c1996a1e2eab3178ad2f6445a5c78b5f
xiepengfeijing <734332894@qq.com>
2018-07-08 15:40:58 +0800

    侧边栏结构完成

--- src/views/home.vue
+++ src/views/home.vue
@@ -14,7 +14,72 @@
           </el-row>
       </el-header>
       <el-container>
-        <el-aside class="aside" width="200px">Aside</el-aside>
+        <el-aside class="aside" width="200px">
+            <el-menu
+              class="menu"
+              :unique-opened="true"
+              :router="true">
+            <el-submenu index="1">
+                <template slot="title">
+                    <i class="el-icon-star-off"></i>
+                    <span>用户管理</span>
+                </template>
+            <el-menu-item index="/users">
+                <i class="el-icon-menu"></i>
+                用户列表
+            </el-menu-item>
+            </el-submenu>
+            <el-submenu index="2">
+                <template slot="title">
+                    <i class="el-icon-star-off"></i>
+                    <span>权限管理</span>
+                </template>
+            <el-menu-item index="/roles">
+                 <i class="el-icon-menu"></i>
+                角色列表
+            </el-menu-item>
+            <el-menu-item index="/rights">
+                 <i class="el-icon-menu"></i>
+                权限列表
+            </el-menu-item>
+            </el-submenu>
+            <el-submenu index="3">
+                <template slot="title">
+                    <i class="el-icon-star-off"></i>
+                    <span>商品管理</span>
+                </template>
+            <el-menu-item index="3-1">
+                 <i class="el-icon-menu"></i>
+                用户列表
+            </el-menu-item>
+            <el-menu-item index="3-2">
+                 <i class="el-icon-menu"></i>
+                分类参数
+            </el-menu-item>
+            </el-submenu>
+            <el-submenu index="4">
+                <template slot="title">
+                    <i class="el-icon-star-off"></i>
+                    <span>订单管理</span>
+                </template>
+            <el-menu-item index="4-1">
+                 <i class="el-icon-menu"></i>
+                用户列表
+            </el-menu-item>
+            </el-submenu>
+            <el-submenu index="5">
+                <template slot="title">
+                    <i class="el-icon-star-off"></i>
+                    <span>数据统计</span>
+                </template>
+            <el-menu-item index="5-1">
+                 <i class="el-icon-menu"></i>
+                数据报表
+            </el-menu-item>
+            </el-submenu>
+            
+            </el-menu>
+        </el-aside>
         <el-main class="main">Main</el-main>
       </el-container>
     </el-container>
@@ -46,6 +111,9 @@ export default {
     }
     .aside {
       background-color: #d3dce6;
+    }
+    .aside .menu {
+        height: 100%;
     }
     .main {
       background-color: #e9eef3;
