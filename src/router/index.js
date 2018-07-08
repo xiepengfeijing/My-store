5fb3252c436f874510f0280ff1e8a9b651594781
xiepengfeijing <734332894@qq.com>
2018-07-08 15:00:09 +0800

    Home整体布局

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Login from '@/views/login';
+import Home from '@/views/home';
 
 Vue.use(Router);
 
@@ -14,7 +15,7 @@ export default new Router({
     {
       name: 'home',
       path: '/',
-      redirect: {name: 'login'}
+      component: Home
     }
   ]
 });
