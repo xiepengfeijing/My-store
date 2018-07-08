7f8820bd77016ea1c9b41e445536e878f918ec2f
xiepengfeijing <734332894@qq.com>
2018-07-08 15:13:46 +0800

    Home的头部

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,18 @@
 <template>
     <el-container class="container">
-      <el-header class="header">Header</el-header>
+      <el-header class="header">
+          <el-row>
+              <el-col :span="4">
+                  <img src="/static/logo.png" alt="">
+              </el-col>
+              <el-col class="middle" :span="19">
+                  <h2>电商后台管理系统</h2>
+              </el-col>
+              <el-col :span="1">
+                  <a class="logout" href="#">退出</a>
+              </el-col>
+          </el-row>
+      </el-header>
       <el-container>
         <el-aside class="aside" width="200px">Aside</el-aside>
         <el-main class="main">Main</el-main>
@@ -20,12 +32,22 @@ export default {
     }
     .header {
       background-color: #b3c0d1;
+      padding: 0;
+    }
+    .header .middle {
+        line-height: 60px;
+        color: #fff;
+        text-align: center;
+    }
+    .header .logout {
+        line-height: 60px;
+        text-decoration: none;
+        color: orange;
     }
     .aside {
       background-color: #d3dce6;
     }
     .main {
       background-color: #e9eef3;
-      height: 100%;
     }
 </style>
